ff1c58dde9efe603c886e10a41f4907dcd0cdf3b
Chris Francis <chrisfrancischris@gmail.com>
2020-05-25 19:24:37 +0530

    Attempt to fix issue with dynamically changing data and loading chart

--- script.js
+++ script.js
@@ -976,7 +976,7 @@ schema = [{
         element.type = "text/javascript";
         element.id = "data";
         document.getElementsByTagName("body")[0].appendChild(element);
-        setTimeout(changeScript, 1500, value );
+        setTimeout(changeScript, 1700, value );
         loadChartData();
         console.log(runID);
         var buttons = document.getElementsByClassName('scenario');

1632c7d620ae0350619d1b48f222e13c1fa9b6fd
Chris Francis <chrisfrancischris@gmail.com>
2020-06-03 16:12:08 +0530

    Add daily numbers for Karnataka districts

--- Karnataka/plot_karnataka.py
+++ Karnataka/plot_karnataka.py
@@ -1,10 +1,22 @@
 import json
+import pandas as pd
+
+def daily(df):
+    for column in df.columns:
+        temp = df[column].sub(df[column].shift())
+        temp.iloc[0] = df[column].iloc[0]
+        df[column] = temp
+    return df
 
 # load the json data
 f = open('districts_daily.json') 
 loaded_json = json.load(f)
 
 data = {}
+dates = []
+
+for entry in loaded_json['districtsDaily']['Karnataka']['Bagalkote']:
+    dates.append(entry['date'])
 
 for district in loaded_json['districtsDaily']['Karnataka'].keys():
     data[district] = []
@@ -12,8 +24,31 @@ for district in loaded_json['districtsDaily']['Karnataka'].keys():
         for status in ['active', 'confirmed', 'deceased', 'recovered']:
             data[district].append([entry['date'], status, entry[status]])
 
+dataframes = {}
+
+for status in ['active', 'confirmed', 'deceased', 'recovered']:
+    dataframes[status] = pd.DataFrame();
+    dataframes[status]['index'] = dates
+    dataframes[status].set_index('index', inplace=True)
+    for district in loaded_json['districtsDaily']['Karnataka'].keys():
+        dataframes[status][district] = 0
+        for entry in loaded_json['districtsDaily']['Karnataka'][district]:
+            dataframes[status].loc[entry['date'], district] = entry[status]
+
+dataframes['active'] = daily(dataframes['active'])
+dataframes['confirmed'] = daily(dataframes['confirmed'])
+dataframes['deceased'] = daily(dataframes['deceased'])
+dataframes['recovered'] = daily(dataframes['recovered'])
+
+dailyData = {}
+
+for district in loaded_json['districtsDaily']['Karnataka'].keys():
+    dailyData[district] = []
+    for entry in loaded_json['districtsDaily']['Karnataka'][district]:
+        for status in ['active', 'confirmed', 'deceased', 'recovered']:
+            dailyData[district].append([entry['date'], status, dataframes[status].loc[entry['date'], district]])
 
 with open("data.js", 'w') as file:
-    file.write("var data = " + str(data) + ";")
+    file.write("var data = " + str(data) + ";"+"var dailyData = " + str(dailyData) + ";")
 
 print("\nData written into data.js")
--- Karnataka/script.js
+++ Karnataka/script.js
@@ -2,14 +2,21 @@
 var currentDistrict = "Bengaluru";
 
 var stateDropDown = document.getElementById("myselect");
+var stateDropDown3 = document.getElementById("myselect3");
 var district;
 for (district of Object.keys(data)){
     stateDropDown.innerHTML += "<option value='"+ district +"'>" + district + "</option>";
+    stateDropDown3.innerHTML += "<option value='"+ district +"'>" + district + "</option>";
 }
 
 function getSelected(){
     var selectedSource = document.getElementById("myselect").value;
     loadChart(selectedSource);
+}
+
+function getSelected3(){
+  var selectedSource = document.getElementById("myselect3").value;
+  loadChart3(selectedSource);
 }
 
 let schema = [{
@@ -75,6 +82,61 @@ let schema = [{
       width: "100%",
       height: L.Browser.mobile?(window.innerHeight/2).toString(): (window.innerHeight - 140).toString() ,
       dataSource: dataSource
+    }).render();
+  
+  }
+
+  var dataStore3 = new FusionCharts.DataStore();
+ var dataSource3 = {
+    chart: {palettecolors: "E41A1C,FF7F00,984EA3,4DAF4A,A65628,F781BF,111111,999999,0069D9",
+            exportEnabled: "1",
+            
+  },
+    caption: {
+      text: currentDistrict
+    },
+    // subcaption: {
+    //   text: currentDistrict
+    // },
+    series: "Type",
+    yaxis: [
+      {
+        plot: "Cases",
+        title: "Cases",
+        // format: {
+        //   prefix: ""
+        // }
+      }
+    ]
+  };
+
+  dataSource3.data = dataStore3.createDataTable(dailyData[currentDistrict], schema);
+  
+  new FusionCharts({
+    type: "timeseries",
+    renderAt: "daily-numbers",
+    width: "100%",
+    height: L.Browser.mobile?(window.innerHeight/2).toString(): (window.innerHeight - 140).toString() ,
+    dataSource: dataSource3
+  }).render();
+
+
+  function loadChart3(district){
+    if(district){
+        district = district;
+    }
+    else{
+        district = "Total";
+    }
+    dataSource3.caption.text = district;
+    dataSource3.data = dataStore3.createDataTable(dailyData[district], schema);
+    
+    new FusionCharts({
+      type: "timeseries",
+      renderAt: "daily-numbers",
+      width: "100%",
+      height: L.Browser.mobile?(window.innerHeight/2).toString(): (window.innerHeight - 140).toString() ,
+      dataSource: dataSource3
     }).render();
   
   }

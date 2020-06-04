9df2067f7d1379864e51477237706c606579927f
Chris Francis <chrisfrancischris@gmail.com>
2020-06-04 12:58:27 +0530

    Add linear regression plot and coefficients for ratios

--- Karnataka/plot_karnataka.py
+++ Karnataka/plot_karnataka.py
@@ -1,6 +1,7 @@
 import json
 import pandas as pd
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
 def daily(df):
     for column in df.columns:
@@ -13,6 +14,7 @@ def daily(df):
 f = open('districts_daily.json') 
 loaded_json = json.load(f)
 
+number_of_dates = {}
 data = {}
 dates = []
 
@@ -21,6 +23,7 @@ for entry in loaded_json['districtsDaily']['Karnataka']['Bagalkote']:
 
 for district in loaded_json['districtsDaily']['Karnataka'].keys():
     data[district] = []
+    number_of_dates[district] = len(loaded_json['districtsDaily']['Karnataka'][district])
     for entry in loaded_json['districtsDaily']['Karnataka'][district]:
         for status in ['active', 'confirmed', 'deceased', 'recovered']:
             data[district].append([entry['date'], status, entry[status]])
@@ -74,7 +77,55 @@ for district in loaded_json['districtsDaily']['Karnataka'].keys():
         for status in ['active', 'confirmed', 'deceased', 'recovered']:
             dailyData[district].append([entry['date'], status, dataframes[status].loc[entry['date'], district]])
 
+results = {}
+
+dataframes['linear-1'] = dataframes['ratio-1'].copy()
+dataframes['linear-2'] = dataframes['ratio-2'].copy()
+dataframes['linear-3'] = dataframes['ratio-3'].copy()
+dataframes['linear-4'] = dataframes['ratio-4'].copy()
+dataframes['linear-5'] = dataframes['ratio-5'].copy()
+
+dataframes['ratio-1'].replace('', 0, inplace=True)
+dataframes['ratio-2'].replace('', 0, inplace=True)
+dataframes['ratio-3'].replace('', 0, inplace=True)
+dataframes['ratio-4'].replace('', 0, inplace=True)
+dataframes['ratio-5'].replace('', 0, inplace=True)
+
+for status in ['ratio-1', 'ratio-2', 'ratio-3', 'ratio-4', 'ratio-5']:
+    results[status] = {}
+    linear = status.replace('ratio', 'linear')
+    for district in loaded_json['districtsDaily']['Karnataka'].keys():
+        results[status][district] = []
+        n = round(44/10)
+        y = dataframes[status][district]
+        dataframes[status]['x'] = [k for k in range(len(dataframes[status].index))]
+        x = dataframes[status]['x']
+        for i in range(1,n):
+            regressor = LinearRegression()
+            regressor.fit(x[(i-1)*10:i*10].to_numpy().reshape(-1, 1), y[(i-1)*10:i*10].to_numpy().reshape(-1, 1))
+            results[status][district].append([regressor.coef_[0][0], regressor.intercept_[0]])
+            for h in range((i-1)*10, i*10):
+                dataframes[linear][district][h] = h*regressor.coef_[0][0] + regressor.intercept_[0]
+        regressor = LinearRegression()
+        regressor.fit(x[(n-1)*10:].to_numpy().reshape(-1, 1), y[(n-1)*10:].to_numpy().reshape(-1, 1))
+        results[status][district].append([regressor.coef_[0][0], regressor.intercept_[0]])
+        for h in range((n-1)*10, len(dataframes[linear][district])):
+            dataframes[linear][district][h] = h*regressor.coef_[0][0] + regressor.intercept_[0]
+
+print(regressor.coef_, regressor.intercept_)
+
+dataframes['linear-1'].replace([np.inf, -np.inf, np.nan,], '', inplace=True)
+dataframes['linear-2'].replace([np.inf, -np.inf, np.nan,], '', inplace=True)
+dataframes['linear-3'].replace([np.inf, -np.inf, np.nan,], '', inplace=True)
+dataframes['linear-4'].replace([np.inf, -np.inf, np.nan,], '', inplace=True)
+dataframes['linear-5'].replace([np.inf, -np.inf, np.nan,], '', inplace=True)
+
+for district in loaded_json['districtsDaily']['Karnataka'].keys():
+    for entry in loaded_json['districtsDaily']['Karnataka'][district]:
+        for status in ['linear-1', 'linear-2', 'linear-3', 'linear-4', 'linear-5']:
+            diagnosticsData[district].append([entry['date'], status, dataframes[status].loc[entry['date'], district]])
+
 with open("data.js", 'w') as file:
-    file.write("var data = " + str(data) + ";"+"var dailyData = " + str(dailyData) + ";"+"var diagnosticsData = " + str(diagnosticsData) + ";")
+    file.write("var data = " + str(data) + ";"+"var dailyData = " + str(dailyData) + ";"+"var diagnosticsData = " + str(diagnosticsData) + ";"+"var results = " + str(results) + ";")
 
 print("\nData written into data.js")
--- Karnataka/script.js
+++ Karnataka/script.js
@@ -11,6 +11,34 @@ for (district of Object.keys(data)){
     stateDropDown3.innerHTML += "<option value='"+ district +"'>" + district + "</option>";
 }
 
+var result = document.getElementById('results');
+result.innerHTML = '<b>Ratio-1</b><br>' 
+                    + '<b>Apr 21-Apr 30:</b> ' + results['ratio-1'][currentDistrict][0]+',<br>'
+                    + '<b>May 1-May 10:</b> ' + results['ratio-1'][currentDistrict][1]+',<br>'
+                    + '<b>May 11-May 20:</b> ' + results['ratio-1'][currentDistrict][2]+',<br>'
+                    + '<b>May 21-Jun 3:</b> ' + results['ratio-1'][currentDistrict][3]+',<br>'
+                    + '<b>Ratio-2</b><br>' 
+                    + '<b>Apr 21-Apr 30:</b> ' + results['ratio-2'][currentDistrict][0]+',<br>'
+                    + '<b>May 1-May 10:</b> ' + results['ratio-2'][currentDistrict][1]+',<br>'
+                    + '<b>May 11-May 20:</b> ' + results['ratio-2'][currentDistrict][2]+',<br>'
+                    + '<b>May 21-Jun 3:</b> ' + results['ratio-2'][currentDistrict][3]+',<br>'
+                    + '<b>Ratio-3</b><br>' 
+                    + '<b>Apr 21-Apr 30:</b> ' + results['ratio-3'][currentDistrict][0]+',<br>'
+                    + '<b>May 1-May 10:</b> ' + results['ratio-3'][currentDistrict][1]+',<br>'
+                    + '<b>May 11-May 20:</b> ' + results['ratio-3'][currentDistrict][2]+',<br>'
+                    + '<b>May 21-Jun 3:</b> ' + results['ratio-3'][currentDistrict][3]+',<br>'
+                    + '<b>Ratio-4</b><br>' 
+                    + '<b>Apr 21-Apr 30:</b> ' + results['ratio-4'][currentDistrict][0]+',<br>'
+                    + '<b>May 1-May 10:</b> ' + results['ratio-4'][currentDistrict][1]+',<br>'
+                    + '<b>May 11-May 20:</b> ' + results['ratio-4'][currentDistrict][2]+',<br>'
+                    + '<b>May 21-Jun 3:</b> ' + results['ratio-4'][currentDistrict][3]+',<br>'
+                    + '<b>Ratio-5</b><br>' 
+                    + '<b>Apr 21-Apr 30:</b> ' + results['ratio-5'][currentDistrict][0]+',<br>'
+                    + '<b>May 1-May 10:</b> ' + results['ratio-5'][currentDistrict][1]+',<br>'
+                    + '<b>May 11-May 20:</b> ' + results['ratio-5'][currentDistrict][2]+',<br>'
+                    + '<b>May 21-Jun 3:</b> ' + results['ratio-5'][currentDistrict][3]+',<br>';
+
+
 function getSelected(){
     var selectedSource = document.getElementById("myselect").value;
     loadChart(selectedSource);
@@ -145,6 +173,33 @@ let schema = [{
       height: L.Browser.mobile?(window.innerHeight/2).toString(): (window.innerHeight - 140).toString() ,
       dataSource: dataSource2
     }).render();
+
+    result = document.getElementById('results');
+    result.innerHTML = '<b>Ratio-1</b><br>' 
+                    + '<b>Apr 21-Apr 30:</b> ' + results['ratio-1'][district][0]+',<br>'
+                    + '<b>May 1-May 10:</b> ' + results['ratio-1'][district][1]+',<br>'
+                    + '<b>May 11-May 20:</b> ' + results['ratio-1'][district][2]+',<br>'
+                    + '<b>May 21-Jun 3:</b> ' + results['ratio-1'][district][3]+',<br>'
+                    + '<b>Ratio-2</b><br>' 
+                    + '<b>Apr 21-Apr 30:</b> ' + results['ratio-2'][district][0]+',<br>'
+                    + '<b>May 1-May 10:</b> ' + results['ratio-2'][district][1]+',<br>'
+                    + '<b>May 11-May 20:</b> ' + results['ratio-2'][district][2]+',<br>'
+                    + '<b>May 21-Jun 3:</b> ' + results['ratio-2'][district][3]+',<br>'
+                    + '<b>Ratio-3</b><br>' 
+                    + '<b>Apr 21-Apr 30:</b> ' + results['ratio-3'][district][0]+',<br>'
+                    + '<b>May 1-May 10:</b> ' + results['ratio-3'][district][1]+',<br>'
+                    + '<b>May 11-May 20:</b> ' + results['ratio-3'][district][2]+',<br>'
+                    + '<b>May 21-Jun 3:</b> ' + results['ratio-3'][district][3]+',<br>'
+                    + '<b>Ratio-4</b><br>' 
+                    + '<b>Apr 21-Apr 30:</b> ' + results['ratio-4'][district][0]+',<br>'
+                    + '<b>May 1-May 10:</b> ' + results['ratio-4'][district][1]+',<br>'
+                    + '<b>May 11-May 20:</b> ' + results['ratio-4'][district][2]+',<br>'
+                    + '<b>May 21-Jun 3:</b> ' + results['ratio-4'][district][3]+',<br>'
+                    + '<b>Ratio-5</b><br>' 
+                    + '<b>Apr 21-Apr 30:</b> ' + results['ratio-5'][district][0]+',<br>'
+                    + '<b>May 1-May 10:</b> ' + results['ratio-5'][district][1]+',<br>'
+                    + '<b>May 11-May 20:</b> ' + results['ratio-5'][district][2]+',<br>'
+                    + '<b>May 21-Jun 3:</b> ' + results['ratio-5'][district][3]+',<br>';
   
   }
 
